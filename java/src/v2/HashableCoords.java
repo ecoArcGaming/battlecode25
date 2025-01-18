d9f6be885dc4d0dec119fa5eea2b9aa2bfa07ac9
ecoArcGaming <erik304501@gmail.com>
2025-01-18 10:45:29 -0500

    encoded SRP explicitly

---
+++ java/src/v2/HashableCoords.java
@@ -0,0 +1,24 @@
+package v2;
+
+public class HashableCoords {
+    private final int x;
+    private final int y;
+
+    public HashableCoords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HashableCoords point = (HashableCoords) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+}
--- java/src/v2/Helper.java
+++ java/src/v2/Helper.java
@@ -3,5 +3,15 @@ package v2;
 import battlecode.common.*;
 
 public class Helper {
+    public static PaintType resourcePatternGrid(RobotController rc, MapLocation loc) {
+        int x = loc.x % 4;
+        int y = loc.y % 4;
+        HashableCoords coords = new HashableCoords(x, y);
+        if (Constants.primarySRP.contains(coords)) {
+            return PaintType.ALLY_PRIMARY;
+        } else {
+            return PaintType.ALLY_SECONDARY;
+        }
 
+    }
 }
